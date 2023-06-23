2c8b481e26a29366b9100c7e6b1d22c3a08b59b1
KrozhDev <roko.mdn@gmail.com>
2023-06-23 08:43:01 +0300

    client has been deleted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,50 @@
+import org.json.JSONObject;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    private static final String HOST = "localhost";
     private static final int PORT = 8989;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        try (Socket clientSocket = new Socket(HOST,PORT);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
-        {
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
 
-            out.println("soft skills microsoft");
-            String response = in.readLine();
-            System.out.println(response);
+        try (ServerSocket serverSocket = new ServerSocket(PORT);) {
+            while (true) {
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                ) {
+                    List<PageEntry> response;
+                    String word = in.readLine().toLowerCase();
+                    response = engine.search(word);
+                    List<String> responseAsJson = getResponseAsJsonFormat(response);
+                    out.println(responseAsJson);
+                }
+            }
+        } catch (
+                IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
         }
+
+    }
+
+    private static List<String> getResponseAsJsonFormat(List<PageEntry> response) {
+        List<String> responseAsJson = new ArrayList<>();
+
+        for (PageEntry pageEntry: response) {
+            JSONObject jsonResponse = new JSONObject();
+            jsonResponse.put("pdfName", pageEntry.getPdfName());
+            jsonResponse.put("page", pageEntry.getPage());
+            jsonResponse.put("count", pageEntry.getCount());
+            responseAsJson.add(jsonResponse.toString());
+        }
+        return responseAsJson;
     }
 }
