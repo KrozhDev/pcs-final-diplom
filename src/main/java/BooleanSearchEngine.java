621a60249d49f9db89c4d055388594091dcaffa7
KrozhDev <roko.mdn@gmail.com>
2023-06-25 17:25:12 +0300

    code refactoring

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -3,6 +3,7 @@ import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -10,29 +11,39 @@ import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
 
-    public static Map<String, ArrayList<PageEntry>> indexingMap = new HashMap<>();
+    private final Map<String, List<PageEntry>> indexingMap = new HashMap<>();
+    private final List<String> stopWords;
 
-    public BooleanSearchEngine(File pdfsDir) throws IOException {
-        File[] pdfFiles = pdfsDir.listFiles();
-        for (File pdfFile : pdfFiles) {
-            if (pdfFile.isFile()) {
-                var pdfDocument = new PdfDocument(new PdfReader(pdfFile));
-                for (int page = 1; page <= pdfDocument.getNumberOfPages(); page++) {
-                    var text = PdfTextExtractor.getTextFromPage(pdfDocument.getPage(page));
-                    var words = text.split("\\P{IsAlphabetic}+");
-                    Map<String, Integer> freqs = getFreqMapFromArrayOfWords(words);
-                    addWordsToIndexingMap(pdfFile, page, freqs);
+    public BooleanSearchEngine(File pdfsDir) {
+        try {
+            File[] pdfFiles = pdfsDir.listFiles();
+            for (File pdfFile : pdfFiles) {
+                if (pdfFile.isFile()) {
+                    var pdfDocument = new PdfDocument(new PdfReader(pdfFile));
+                    for (int page = 1; page <= pdfDocument.getNumberOfPages(); page++) {
+                        var text = PdfTextExtractor.getTextFromPage(pdfDocument.getPage(page));
+                        var words = text.split("\\P{IsAlphabetic}+");
+                        Map<String, Integer> freqs = getFreqMapFromArrayOfWords(words);
+                        addWordsToIndexingMap(pdfFile, page, freqs);
+                    }
                 }
             }
+
+            ReaderImpl readerImpl = new ReaderImpl("stop-ru.txt");
+            this.stopWords = readerImpl.getWords();
+
+        } catch (IOException e) {
+            System.err.println("In catch IOException: " + e.getClass());
+            throw new RuntimeException();
         }
     }
 
-    private static void addWordsToIndexingMap(File pdfFile, int page, Map<String, Integer> freqs) {
+    private void addWordsToIndexingMap(File pdfFile, int page, Map<String, Integer> freqs) {
         for (String word : freqs.keySet()) {
             PageEntry pageEntry = new PageEntry(pdfFile.getName(), page, freqs.get(word));
 
             indexingMap.compute(word, (k, v) -> {
-                ArrayList<PageEntry> pageEntries = new ArrayList<>();
+                List<PageEntry> pageEntries = new ArrayList<>();
                 if (v == null) {
                     v = pageEntries;
                 }
@@ -42,7 +53,7 @@ public class BooleanSearchEngine implements SearchEngine {
         }
     }
 
-    private static Map<String, Integer> getFreqMapFromArrayOfWords(String[] words) {
+    private Map<String, Integer> getFreqMapFromArrayOfWords(String[] words) {
         Map<String, Integer> freqs = new HashMap<>();
         for (var word : words) {
             if (word.isEmpty()) {
@@ -56,49 +67,42 @@ public class BooleanSearchEngine implements SearchEngine {
 
     @Override
     public List<PageEntry> search(String sentence) {
-
-        try {
-            ReaderImpl readerImpl = new ReaderImpl("stop-ru.txt");
-            List<String> stopWords = readerImpl.getWords();
-            List<String> words = new ArrayList<>(Arrays.asList(sentence.split(" ")));
-            List<String> wordsToDelete = new ArrayList<>();
-            for (String word : words) {
-                if (stopWords.contains(word)) {
-                    wordsToDelete.add(word);
-                }
+        List<String> words = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+        List<String> wordsToDelete = new ArrayList<>();
+        for (String word : words) {
+            if (stopWords.contains(word)) {
+                wordsToDelete.add(word);
             }
-
-            Iterator<String> wordIterator = words.iterator();
-            while (wordIterator.hasNext()) {
-                String nextWord = wordIterator.next();
-                if (stopWords.contains(nextWord)) {
-                    wordIterator.remove();
-                }
-            }
-
-            Map<PageEntry, Integer> pageEntries = new HashMap<>();
-
-
-            for (String word : words) {
-                List<PageEntry> nextWordPageEntries = indexingMap.get(word);
-                for (PageEntry pageEntry : nextWordPageEntries) {
-                    pageEntries.compute(pageEntry, (k, v) -> {
-                        if (v == null) {
-                            v = k.getCount();
-                        } else {
-                            v += k.getCount();
-                        }
-                        return v;
-                    });
-                }
-            }
-
-            return pageEntries.entrySet().stream()
-                    .map(e -> new PageEntry(e.getKey().getPdfName(), e.getKey().getPage(), e.getValue()))
-                    .sorted()
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+
+        Iterator<String> wordIterator = words.iterator();
+        while (wordIterator.hasNext()) {
+            String nextWord = wordIterator.next();
+            if (stopWords.contains(nextWord)) {
+                wordIterator.remove();
+            }
+        }
+
+        Map<PageEntry, Integer> pageEntries = new HashMap<>();
+
+
+        for (String word : words) {
+            List<PageEntry> nextWordPageEntries = indexingMap.get(word);
+            for (PageEntry pageEntry : nextWordPageEntries) {
+                pageEntries.compute(pageEntry, (k, v) -> {
+                    if (v == null) {
+                        v = k.getCount();
+                    } else {
+                        v += k.getCount();
+                    }
+                    return v;
+                });
+            }
+        }
+
+        return pageEntries.entrySet().stream()
+                .map(e -> new PageEntry(e.getKey().getPdfName(), e.getKey().getPage(), e.getValue()))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
